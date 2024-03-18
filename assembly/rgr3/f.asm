d4c04a1325f196dbbc6ec991315ba7babdc2a570
ranopashec <110613416+ranopashec@users.noreply.github.com>
2024-03-18 19:16:48 +0300

    Update f.asm

--- assembly/rgr3/f.asm
+++ assembly/rgr3/f.asm
@@ -1,22 +1,26 @@
      .386
-	 PUBLIC _Merge1@20
+	 PUBLIC _Merge1@20 ; ПОСЛЕ СОБАКИ СКОЛЬКО БАЙТ ЗАНИМАЮТ АРГУМЕНТЫ
 	 PUBLIC _Merge2
      PUBLIC Merge3
 	 PUBLIC Merge4
-	 PUBLIC @Merge5@20
+	 PUBLIC @Merge5@20 ; ПОСЛЕ СОБАКИ СКОЛЬКО БАЙТ ЗАНИМАЮТ АРГУМЕНТЫ
 
 	.model flat
 	.data
 	.code
 
-	_Merge1@20 proc 
+	_Merge1@20 proc ; 20 = 4 * 5 так как у меня 5 аргументов
+        ; ------------------------------ дальше код моей задачи, который вам надо поменять
+
+					; ВО ВСЕХ ПРОЦЕДУРАХ СОЗДАËМ ЛОКАЛЬНУЮ 
+ 					; ПЕРЕМЕННУЮ ЧТОБЫ НЕ ПИСАТЬ ПРОЛОГИ ЭПИЛОГИ
 	        local n:dword,
 			m:dword,
 			cn:dword,
 			ar:dword,
 			br:dword,
 			answ:dword
-
+					; АРГУМЕНТЫ ПРИХОДЯТ ИЗ СТЭКА НАЧИНАЯ С [EBP+8]
 		mov eax, [ebp+8]
 		mov n, eax
 		mov eax, [ebp+12]
@@ -41,7 +45,8 @@
 		cmp edi, m
 		jge lupa
 		mov edx, ar
-		mov eax, [edx+esi*4]
+		mov eax, [edx+esi*4]      ; ЗАМЕТЬТЕ НЕЛЬЗЯ ОБРАЩАТЬСЯ К МАССИВУ НЕ ЧЕРЕЗ РЕГИСТР
+ 					  ; ТОЛЬКО ВОТ ТАК КАК ТУТ МОЖНО
 		mov edx, br
 		mov ebx, [edx+edi*4]
 		cmp eax, ebx
@@ -83,13 +88,14 @@
 		jmp lupb
 	endend:
 	    xor edx, edx
-			ret 20
-			_Merge1@20 endp
+	; ------------------------
+			ret 20     	; 20 = 4 * кол-во аргументов
+			_Merge1@20 endp ; 20 = 4 * кол-во аргументов
 
 			
 	;//////////////////////////////////////////////////////////////////
 
-	_Merge2 proc
+	_Merge2 proc			; ТО ЖЕ САМОЕ только В ret ничего не пишем и никаких цифр с собачками
 			local n:dword,
 			m:dword,
 			cn:dword,
@@ -163,9 +169,12 @@
 		jmp lupb
 	endend:
 	    xor edx, edx
-			ret
-			_Merge2 endp
+			
+	ret
+	_Merge2 endp
 
+;//////////////////////////////////////////////////////////////////
+; ТУТ АРГУМЕНТЫ ПИШЕМ СРАЗУ ВВЕРХУ, но не забываем создать local переменную чтобы не париться с прологом эпилогом
 	Merge3   proc stdcall, n:dword, m:dword, ar:dword, br:dword, answ:dword
 		local cn:dword
 		mov eax, n
@@ -227,10 +236,10 @@
 	    xor edx, edx
 			ret 
 			Merge3 endp
+;//////////////////////////////////////////////////////////////////
+; ТО ЖЕ САМОЕ, только другой тип фунцкции в c++ файле и в объявлении
 
-			;/////////////////////////
-
-			Merge4   proc C, n:dword, m:dword, ar:dword, br:dword, answ:dword
+		Merge4   proc C, n:dword, m:dword, ar:dword, br:dword, answ:dword
 		local cn:dword
 		mov eax, n
 		add eax, m
@@ -289,12 +298,12 @@
 		jmp lupb
 	endend:
 	    xor edx, edx
-			ret 
-			Merge4 endp
+		ret 
+		Merge4 endp
 
-	;//////////////////////////////////////////////////////////
-
-	@Merge5@20 proc
+//////////////////////////////////////////////////////////////////
+; Как и в 1, но первые два аргументы лежат в ecx и еdx
+	@Merge5@20 proc ; 20 = 4 * кол-во аргументов
 			local n:dword,
 			m:dword,
 			cn:dword,
@@ -368,6 +377,6 @@
 		jmp lupb
 	endend:
 	    xor edx, edx
-	    ret 12
-	@Merge5@20 endp
+	    ret 12 ; 12 = 4 * (кол-во аргументов - 2)
+	@Merge5@20 endp ; 20 = 4 *  кол-во аргументов
 	end
